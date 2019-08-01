649cbbee29cd81f9a6df684b7f328fefa8ee82e1
aayushtibra <aayushtibra1997@gmail.com>
2019-08-01 14:24:43 +0530

    second commit

---
+++ build/precache-manifest.39c6e8132be68314122b4da1f26edd9d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dc480f86f8c26d3e51b19ab3d186e6c",
+    "url": "/ayushtibra.github.io/index.html"
+  },
+  {
+    "revision": "ee95bec6e239899f2f78",
+    "url": "/ayushtibra.github.io/static/css/3.140ecb1d.chunk.css"
+  },
+  {
+    "revision": "f8e96ce779900e387ad9",
+    "url": "/ayushtibra.github.io/static/css/4.26d21c9e.chunk.css"
+  },
+  {
+    "revision": "a7c837b9abb5e96782d0",
+    "url": "/ayushtibra.github.io/static/css/5.5fcf6bb4.chunk.css"
+  },
+  {
+    "revision": "7243fa2175b56c091e7e",
+    "url": "/ayushtibra.github.io/static/css/main.7398e15d.chunk.css"
+  },
+  {
+    "revision": "fabb46d9f6ab32627083",
+    "url": "/ayushtibra.github.io/static/js/2.068c0f6e.chunk.js"
+  },
+  {
+    "revision": "ee95bec6e239899f2f78",
+    "url": "/ayushtibra.github.io/static/js/3.4acf83c9.chunk.js"
+  },
+  {
+    "revision": "f8e96ce779900e387ad9",
+    "url": "/ayushtibra.github.io/static/js/4.bb2273ab.chunk.js"
+  },
+  {
+    "revision": "a7c837b9abb5e96782d0",
+    "url": "/ayushtibra.github.io/static/js/5.8bc9db67.chunk.js"
+  },
+  {
+    "revision": "7243fa2175b56c091e7e",
+    "url": "/ayushtibra.github.io/static/js/main.50b5e9ac.chunk.js"
+  },
+  {
+    "revision": "c3b7757718f586e104a1",
+    "url": "/ayushtibra.github.io/static/js/runtime~main.64bea286.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/ayushtibra.github.io/static/media/burger-logo.b8503d26.png"
+  }
+]);
